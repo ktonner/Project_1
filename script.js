3f93d0771e7a9d70b3d73c19005587f6e36b5010
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-25 11:56:21 -0400

    Dropdown less janky

--- script.js
+++ script.js
@@ -5,21 +5,35 @@
 //     // //AJAX Call to Brewery API
 //     // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
 //     //get the users position from the geolocation API
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-        console.log(lat +"and"+ long);
-        //Query for google places
-        const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
-        //AJAX Call to Places API
-        $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-            console.log(response);
-        });
+navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    console.log(lat + "and" + long);
+    //Query for google places
+    const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
+    //AJAX Call to Places API
+    $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+        console.log(response);
     });
+});
 ;
 
 
-    //Dropdown menu click
-    $(".dropdown").on("click", function(){
-        $(".dropdown").addClass("is-active")
-    })
+//Dropdown Menu JS
+//clicking on the menu opens it
+$(".dropdown").on("click", function () {
+    event.stopPropagation();
+    $(".dropdown").addClass("is-active")
+
+})
+//if you click off the menu it closes
+$(window).click(function () {
+    $(".dropdown").removeClass("is-active")
+});
+
+//clicking on a beer sets the value
+($(".dropdown-item")).on("click", function(){
+    $(event.target).addClass("is-active")
+    beerChoice = event.target.text 
+    console.log($(".dropdown-title").innerHTML)
+})
