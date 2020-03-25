7aed5df7bff0bb8758ff63ed200c82e745f08d16
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-25 18:53:00 -0400

    With Johns help, got the brewery result to come back

--- script.js
+++ script.js
@@ -3,24 +3,52 @@
 
 //get the users position from the geolocation API
 navigator.geolocation.getCurrentPosition((position) => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log(lat + "and" + long);
-    //Query for google places
-    //I commented this url below, idk what it's doing here?? --KATHLEEN
-    //const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
-    //AJAX Call to Places API
-    
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log(position)
+    console.log(lat + " and " + long);
+
     //when the user selects a beer...
-    $(".dropdown-item").on("click", function(){
-        console.log("hello")
-    }
-    )
-    //AJAX Call to Brewery API
-    const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=30ca2231c143bbf6621ca369ec091127&";
-    $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-        console.log(response);
-        //Query for BreweryDB
+    $(".dropdown-item").on("click", function () {
+        var pour = $(".dropdown-item")
+        //get the users position from the geolocation API
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        console.log(lat +" and "+ lng);
+        //call the first API
+        getPlaces(lat,lng,pour);
+    }); 
+
+const getPlaces = (lat,lng) => {
+    //    Query for google places
+        const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=500&types=beer&input="+pour+"&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
+        //AJAX Call to Places API
+        $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+            console.log(response);
+            //get city from response
+            city = response.results[0].name
+            console.log(city)
+            getBreweries(city)
+        })} 
+        const getBreweries=()=>{
+            //!!!! DO NOT TOUCH THIS, I WILL WORK ON THIS TOMORROW - KATHLEEN
+            const queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + "Winter_Springs"
+            console.log(queryURL);
+            //AJAX Call to Brewery API
+            $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+                console.log(response);
+                //ToDo Show the DATA on the HTML
+                //showStuffOnThePage();
+            });   
+}})
+
+       var queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" +long+"&radius=10&key=59f4cee9ab8d9017113fe1a5520db79b";
+       $.ajax({ url: queryURL, 
+            method: 'GET' }).then(function (response) {
+            console.log(response);
+        }
+        )
 
         //THIS IS FOR THE WINE BUTTON --KATHLEEN
         $("#wine").on("click", function () {
@@ -38,30 +66,6 @@ navigator.geolocation.getCurrentPosition((position) => {
             });
         })
     });
-})
-
-
-// document.getElementById("hit").addEventListener("click", (event) => {
-//     //no refresh
-//     event.preventDefault();
-//     //console.log(document.getElementById("test").value);
-//     // //AJAX Call to Brewery API
-//     // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
-//     //get the users position from the geolocation API
-navigator.geolocation.getCurrentPosition((position) => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log(lat + "and" + long);
-    //Query for google places
-    const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
-    //AJAX Call to Places API
-    $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-        console.log(response);
-
-
-    });
-});
-;
 
 
 //Dropdown Menu JS
