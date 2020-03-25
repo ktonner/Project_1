a96f34fec813f01c2a5b8291035f3d56488ea889
Jimmy Smith <jasmith.188@gmail.com>
2020-03-24 21:47:04 -0400

    API request not working

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-document.getElementById("hit").addEventListener("click", (event) => {
+document.getElementById("hit").addEventListener("dropdown-item", event => {
     //no refresh
     event.preventDefault();
-    //console.log(document.getElementById("test").value);
+    console.log(document.getElementById("test").value);
     // //AJAX Call to Brewery API
-    // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
-    //get the users position from the geolocation API
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-        console.log(lat +"and"+ long);
-        //Query for google places
-        const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
-        //AJAX Call to Places API
-        $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-            console.log(response);
+    $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+        console.log(response);
+        //Query for BreweryDB
+        const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=30ca2231c143bbf6621ca369ec091127&";
+        //get the users position from the geolocation API
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+            console.log(lat + "and" + long);
+            //Query for google places
+            const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
+            //AJAX Call to Places API
+            $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+                console.log(response);
+            });
         });
-    });
+    })
+})
