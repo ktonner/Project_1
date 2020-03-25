0b5d6eaf97c016abb286fdc518cc41d230c528fa
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-25 11:28:40 -0400

    Fixed to add click to make dropdown active

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById("hit").addEventListener("click", (event) => {
-    //no refresh
-    event.preventDefault();
-    //console.log(document.getElementById("test").value);
-    // //AJAX Call to Brewery API
-    // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
-    //get the users position from the geolocation API
+// document.getElementById("hit").addEventListener("click", (event) => {
+//     //no refresh
+//     event.preventDefault();
+//     //console.log(document.getElementById("test").value);
+//     // //AJAX Call to Brewery API
+//     // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
+//     //get the users position from the geolocation API
     navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const long = position.coords.longitude;
@@ -16,3 +16,10 @@ document.getElementById("hit").addEventListener("click", (event) => {
             console.log(response);
         });
     });
+;
+
+
+    //Dropdown menu click
+    $(".dropdown").on("click", function(){
+        $(".dropdown").addClass("is-active")
+    })
