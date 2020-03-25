03210d6c67eddf0a191e6c02146e589330c308e0
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-03-25 11:39:34 -0400

    link js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-document.getElementById("hit").addEventListener("click", (event) => {
+$(document).ready(function () {
     //no refresh
-    event.preventDefault();
+   
     //console.log(document.getElementById("test").value);
     // //AJAX Call to Brewery API
-    // const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
+    const queryURL = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/breweries?key=c89b3a27cf9adf60c71b39ce50ffa671&";
     //get the users position from the geolocation API
     navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const long = position.coords.longitude;
-        console.log(lat +"and"+ long);
+        console.log(lat + "and" + long);
         //Query for google places
-        const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
+       // const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
         //AJAX Call to Places API
-        $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+        $.ajax({ 
+            url: queryURL, 
+            method: 'GET' 
+        }).then(function (response) {
             console.log(response);
         });
     });
+})
