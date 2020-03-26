09da62ec6768a4378e6c19499287d623eb5c9084
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-03-26 14:05:36 -0400

    i fix the google API

--- google.js
+++ google.js
@@ -1,39 +1,46 @@
 console.log("hooked up")
 
 $(document).ready(function () {
+    // global variables
+    let lat = 0;
+    let long = 0;
+
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        console.log(lat + "and" + long);
+
+    });
 
 
     $("#wine").click(function (e) {
         e.preventDefault();
+        console.log(lat + "and" + long + "inside of the click");
 
+        var winery = "wine"
+        //Query for google places
+        const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=" + winery + "&key=AIzaSyD9Ff1tE0-VCQ6xdBVIecM05QkaMPvHlGU"
+        //AJAX Call to Places API
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            //console.log(response);
 
-        navigator.geolocation.getCurrentPosition((position) => {
-            const lat = position.coords.latitude;
-            const long = position.coords.longitude;
-            console.log(lat + "and" + long);
-            var winery = "wine"
-            //Query for google places
-            const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=" + winery + "&key=AIzaSyD9Ff1tE0-VCQ6xdBVIecM05QkaMPvHlGU"
-            //AJAX Call to Places API
-            $.ajax({
-                url: queryURL,
-                method: 'GET'
-            }).then(function (response) {
-                //console.log(response);
+            for (var i = 0; i < 5; i++) {
+                var element = response.results[i];
+                console.log(element);
 
-                for (var i = 0; i < 5; i++) {
-                    var element = response.results[i];
-                    console.log(element);
+                var div = $("<p>").addClass("card-name").text(element.name);
+                var location = $("<p>").addClass("card-location").text(element.vicinity);
+                var images1 = $("<img>").attr("src", element.icon)
+                $("#wineSelection").append(div, location, images1); ``
 
-                    var div = $("<p>").addClass("card-name").text(element.name);
-                    var location = $("<p>").addClass("card-location").text(element.vicinity);
-                    var images1 = $("<img>").attr("src", element.icon)
-                    $("#wineSelection").append(div,location,images1);``
-
-                }
-            });
+            }
         });
+    });
 
-    })
-})
+});
+
 
