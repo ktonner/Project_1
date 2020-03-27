1b0ad0f4738d478fa12b63bd42cdfb65ac4756a0
Jimmy Smith <jasmith.188@gmail.com>
2020-03-26 22:41:42 -0400

    clean up scriptjs

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 $(document).ready(function () {
-    //no refresh
-    //When the beer button is clicked
-    $("#beer-btn").on("click", function () {
-        callApi();
+  //no refresh
 
+  //When the beer button is clicked
+  $("#beer-btn").on("click", function () {
+    callApi();
+  });
+})
+function callApi() {
+
+  //get the users position from the geolocation API
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    //display location coordinates
+    console.log(lat + "and" + long);
+    //display info
+    console.log("click")
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" + long + "&radius=40&key=c400eb8346467f09e19ffeff274b1b6a"
+    console.log(queryURL);
+    //AJAX Call to Brewery API
+    $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+      console.log(response);
+      createBeerCards(response.data);
+      //Show stuff on the page();
     });
+  });
+}
+//Create a column to display search results
+function createBeerCards(data) {
+  $("#display-case").empty()
+  data.forEach(beer => {
+    let card = `
+    <div class="card">
+<div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="${beer.brewery.images.icon}" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">${beer.brewery.name}</p>
+          <p class="subtitle is-6">${beer.brewery.description}</p>
+        </div>
+      </div>
+</div>
+  </div>
+    
+    `;
+    $('#display-case').append(card)
+  });
 
 
+}
     //  $("#beer-button").on("click", function(){
     //     for(i = 0; i <= 5, i++;){
     //        var box = $("<container>")
@@ -56,6 +102,12 @@ $(document).ready(function () {
 
 
     // })
+    // //Query for google places
+    // var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
+    // //AJAX Call to Places API
+    // $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+    //     console.log(response);
+    // });
 
 
 
@@ -76,68 +128,3 @@ $(document).ready(function () {
     // $(window).click(function () {
     //     $(".dropdown").removeClass("is-active")
     // })
-
-})
-
-function callApi() {
-    //get the users position from the geolocation API
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-        console.log(lat + "and" + long);
-        // //Query for google places
-        // var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&types=wine&key=AIzaSyBRbn4-RYBysbGOiH11a0r5xu4NLIwM4iA"
-        // //AJAX Call to Places API
-        // $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-        //     console.log(response);
-        // });
-
-
-
-
-        console.log("click")
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" + long + "&radius=40&key=c400eb8346467f09e19ffeff274b1b6a"
-        console.log(queryURL);
-        //AJAX Call to Brewery API
-        $.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
-            console.log(response);
-            createBeerCards(response.data);
-            //ToDo Show the DATA on the HTMLs
-            //showStuffOnThePage();
-        });
-
-
-    });
-
-}
-
-function createBeerCards(data) {
-    $("#display-case").empty()
-    data.forEach(beer => {
-        let card = `
-    <div class="card">
-
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="${beer.brewery.images.icon}" alt="Placeholder image">
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">${beer.brewery.name}</p>
-          <p class="subtitle is-6">${beer.brewery.description}</p>
-        </div>
-      </div>
-
-    </div>
-  </div>
-    
-    `;
-
-        $('#display-case').append(card)
-
-    });
-
-
-}
